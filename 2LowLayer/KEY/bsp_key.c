441a2286088c029df0ebe6b7e2e3dd8c6d446f21
zhangduo <zdsurge@163.com>
2020-02-26 15:35:34 +0800

    init

---
+++ 2LowLayer/KEY/bsp_key.c
@@ -0,0 +1,70 @@
+/******************************************************************************
+
+                  版权所有 (C), 2013-2023, 深圳博思高科技有限公司
+
+ ******************************************************************************
+  文 件 名   : key.c
+  版 本 号   : 初稿
+  作    者   : 张舵
+  生成日期   : 2019年5月25日
+  最近修改   :
+  功能描述   : 键盘驱动
+  函数列表   :
+  修改历史   :
+  1.日    期   : 2019年5月25日
+    作    者   : 张舵
+    修改内容   : 创建文件
+
+******************************************************************************/
+
+#include "bsp_key.h"
+
+
+//按键初始化函数
+void bsp_key_Init ( void )
+{
+	GPIO_InitTypeDef  GPIO_InitStructure;
+
+	RCC_AHB1PeriphClockCmd ( RCC_ALL_KEY, ENABLE ); //使能GPIOE
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_PIN_KEY_DOOR_B; //对应引脚
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;//普通输入模式
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;//100M
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;//
+	GPIO_Init ( GPIO_PORT_KEY, &GPIO_InitStructure ); //初始化GPIOE9
+}
+
+//按键处理函数
+//返回按键值
+//mode:0,不支持连续按;1,支持连续按;
+//0，没有任何按键按下
+//1，KEY_DOOR_B按下
+
+u8 bsp_key_Scan ( u8 mode )
+{
+	static u8 key_up=1;//按键按松开标志
+	if ( mode )
+	{
+		key_up=1;    //支持连按
+	}
+	if ( key_up&& ( KEY_DOOR_B==0 ) )
+	{
+		delay_ms ( 10 ); //去抖动
+		
+		key_up=0;
+        
+		if ( KEY_DOOR_B == 0 )
+		{
+			return KEY_DOOR_B_PRES;
+		}
+
+	}
+	else if ( KEY_DOOR_B == 1 )
+	{
+		key_up=1;
+	}
+    
+	 return KEY_NONE;// 无按键按下
+}
+
+
---
+++ 6Bin/uartqueue.d
@@ -0,0 +1,4 @@
+..\6bin\uartqueue.o: ..\2LowLayer\SUART\UARTQueue.c
+..\6bin\uartqueue.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+..\6bin\uartqueue.o: ..\2LowLayer\SUART\UARTQueue.h
+..\6bin\uartqueue.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
